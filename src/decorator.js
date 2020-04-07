9d3555200b462629f8637427ff244dfea94c8dca
WANGXIAOBO558 <WANGXIAOBO558@pingan.com.cn>
2020-04-07 09:35:50 +0800

    add plugins to resolve decorators

--- src/decorator.js
+++ src/decorator.js
@@ -1,8 +1,14 @@
+function logger (target, name, descriptor) {
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
 class Person {
   constructor(name, age) {
     this.name = name;
     this.age = age;
   }
+  @logger
   sayHi () {
     console.log(`Hi, i am ${this.name} and i am ${this.age} years old.`);
   }
